4e97419612378c50078fa49d8ec8eb90b9c61f9d
Sven-S. Porst <ssp-web@earthlingsoft.net>
2021-01-03 19:26:29 +0100

    make Chromecast name configurable via URL

--- index.js
+++ index.js
@@ -3,25 +3,29 @@ const { spawnSync } = require('child_process')
 const url = require('url');
 
 const castPath = '/Users/ssp/bin/cast'
-const chromecastName = 'Wohnzimmer'
 
 const app = express()
 const port = 10000
 
-app.get('/cast', (req, res) => {
-    const URLStringToCast = req.query['url']
+app.get('/cast/:chromecastName', (request, res) => {
+    const URLStringToCast = request.query['url']
     const URLError = validateURL(URLStringToCast)
     if (URLError) {
         return res.status(400).send(URLError)
     }
 
-    if (!runCastCommand(['quit'])) {
-        res.status(500).send(`Could not quit current application on »${chromecastName}«`)
+    const chromecastName = request.params.chromecastName
+    if (!chromecastName) {
+        return res.status(400).send(`path »${request.path}« needs to contain non-empty Chromecast name after /cast/.`)
+    }
+
+    if (!runCastCommand(chromecastName, ['quit'])) {
+        res.status(500).send(`Could not quit current application on »${chromecastName}«.`)
         return
     }
 
-    if (!runCastCommand(['url', 'load', URLStringToCast])) {
-        res.status(500).send(`Could not show »${URLStringToCast}« on »${chromecastName}«`)
+    if (!runCastCommand(chromecastName, ['url', 'load', URLStringToCast])) {
+        res.status(500).send(`No successful answer from Chromecast within timeout for showing »${URLStringToCast}« on »${chromecastName}«. It may have worked nonetheless.`)
         return
     }
 
@@ -34,18 +38,18 @@ app.listen(port, () => {
 
 function validateURL(URLStringToCast) {
     if (!URLStringToCast) {
-        return '»url« parameter must be non-empty'
+        return '»url« parameter must be non-empty.'
     }
 
     try {
         new url.URL(URLStringToCast)
         return ''
     } catch (e) {
-        return `value »${URLStringToCast}« in parameter »url« is not a valid URL`
+        return `value »${URLStringToCast}« in parameter »url« is not a valid URL.`
     }
 }
 
-function runCastCommand(commandParts) {
+function runCastCommand(chromecastName, commandParts) {
     const baseArguments = ['--name', chromecastName]
     const arguments = baseArguments.concat(commandParts)
     const result = spawnSync(castPath, arguments)
